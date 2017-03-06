eec54a3a2ebfdb448982a695cefd62518ab9496e
andrei lorin <andreilorin@gmail.com>
2017-03-06 20:48:29 +0000

    Created Sprite.class

---
+++ src/main/java/graphics/Sprite.java
@@ -0,0 +1,32 @@
+package graphics;
+
+/**
+ * Created by Lorin on 2017-03-06.
+ */
+public class Sprite {
+
+    public final int SIZE;
+    private int x, y;
+    public int[] pixels;
+    private SpriteSheet sheet;
+
+    public static Sprite grass = new Sprite(16, 0, 0, SpriteSheet.tiles);
+
+    public Sprite(int size, int x, int y, SpriteSheet sheet){
+        SIZE = size;
+        pixels = new int[SIZE * SIZE];
+        this.x = x * size;
+        this.y = y * size;
+        this.sheet = sheet;
+        loadSprite();
+    }
+
+    private void loadSprite(){
+        for(int y = 0; y < SIZE; y++){
+            for(int x = 0; x < SIZE; x++){
+                pixels[x + y * SIZE] = sheet.pixels[(x + this.x) + (y + this.y) * sheet.SIZE];
+            }
+        }
+    }
+
+}
